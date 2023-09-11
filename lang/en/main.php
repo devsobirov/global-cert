1c21d1651c86c98ecf56cd8dc382511a0d4fab22
devsobirov <devsobirov@gmail.com>
2023-09-11 07:52:31 +0500

    translation manager import issue

--- lang/en/main.php
+++ lang/en/main.php
@@ -1,7 +1,5 @@
 <?php
 
-return array (
-  'about' => 'About Us',
-  'nav_gallery' => 'Gallery',
-  'nav_news' => 'News',
-);
+return [
+  'home' => 'Homepage'
+];
--- lang/uz/main.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array (
-  'about' => 'Biz haqimizda',
-  'nav_gallery' => 'Foto va video galeriya',
-  'nav_news' => 'Yangiliklar',
-);

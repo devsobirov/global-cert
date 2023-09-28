88e337cd75fa0ef2104348c78fe4de3f60e18ed2
devsobirov <devsobirov@gmail.com>
2023-09-28 15:35:28 +0300

    production updates

--- lang/en/main.php
+++ lang/en/main.php
@@ -1,6 +1,40 @@
 <?php
 
-return [
+return array (
   'home' => 'Homepage',
   'certificate_not_found' => 'Certificate with № :code not found',
-];
+  'btn_send' => 'Send',
+  'callback_btn' => 'Call Me',
+  'footer_address' => 'Address',
+  'footer_copyright' => 'All rights reserved',
+  'btn_send_msg' => 'Send message',
+  'callback_close' => 'Close',
+  'callback_h2' => 'We have been working for <span>You for more than 5 years for</span> the best results',
+  'callback_phone' => 'Your phone number',
+  'certificate_check' => 'Check certificate',
+  'certificate_check_header' => 'Check certificate header',
+  'certificate_check_info' => 'Check certificate description or instructions',
+  'certificate_number' => 'Certificate number',
+  'contact' => 'Feedback',
+  'contact_company' => 'Your company',
+  'contact_header' => 'Dear visitors of our site!',
+  'contact_info' => 'You have a question, we will be glad to listen to you and will try to answer. <br>
+                                                 We reserve the right not to respond to comments and not to publish letters from visitors that, in our opinion, do not carry a semantic load or otherwise do not need answers.<br>
+                                                 We recommend not to use profanity, as such messages will be deleted automatically.',
+  'contact_message' => 'Message',
+  'contact_name' => 'Your name',
+  'contact_phone' => 'Your Email or phone number:',
+  'footer_header' => 'OUR CONTACTS',
+  'footer_info' => 'It has long been established that when evaluating design and composition, reading text interferes with concentration.',
+  'footer_navigation' => 'SITE NAVIGATION',
+  'header_email' => 'Email',
+  'header_hours' => 'Working hours',
+  'header_phone' => 'Telephone',
+  'news_latest' => 'Last news',
+  'news' => 'Company news',
+  'news_text' => 'A brief description of this block or any text.',
+  'our_projects' => 'Our projects',
+  'our_projects_text' => 'A brief description of this block or any text.',
+  'our_team' => 'Our team',
+  'our_team_text' => 'A brief description of this block or any text.',
+);
